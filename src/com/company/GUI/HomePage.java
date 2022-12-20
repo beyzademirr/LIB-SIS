33596c3b0799c2c77a5e9a93f5cb8d828c9f08b2
Omeraskinn <120595582+Omeraskinn@users.noreply.github.com>
2022-12-21 02:41:49 +0300

    Add files via upload
    HomePage

---
+++ src/com/company/GUI/HomePage.java
@@ -0,0 +1,9 @@
+package com.company.GUI;
+import javax.swing.*;
+public class HomePage {
+
+
+    private JPanel panel1;
+    private JButton profilePageButton;
+    private JButton libraryButton;
+}

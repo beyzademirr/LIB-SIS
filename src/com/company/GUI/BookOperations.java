d539f3f63be3e4b0d48fefc96696ff7373bb4041
selinnyilmaz <101280989+selinnyilmaz@users.noreply.github.com>
2022-12-20 19:20:15 +0300

    User Page and Book Operations added

---
+++ src/com/company/GUI/BookOperations.java
@@ -0,0 +1,18 @@
+package com.company.GUI;
+
+import javax.swing.*;
+
+public class BookOperations {
+    private JButton add;
+    private JButton delete;
+    private JButton modify;
+    private JButton deletex;
+    private JTextField itemId;
+    private JTextField author;
+    private JTextField userId;
+    private JTextField availability;
+    private JTextField year;
+    private JTextField pages;
+    private JTextField loation;
+    private JTextField reservationDate;
+}
---
+++ src/com/company/GUI/UserPage.java
@@ -0,0 +1,10 @@
+package com.company.GUI;
+
+import javax.swing.*;
+
+public class UserPage {
+    private JButton profile;
+    private JTextField reservedId;
+    private JTextField reservationDate;
+    private JTextField dueDate;
+}

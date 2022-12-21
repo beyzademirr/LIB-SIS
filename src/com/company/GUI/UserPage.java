72d61166c9634861cf4806fb16c525aaff03dc12
ctllmp <66204796+ctllmp@users.noreply.github.com>
2022-12-21 21:09:56 +0300

    added functionality to user page

--- src/com/company/GUI/UserPage.java
+++ src/com/company/GUI/UserPage.java
@@ -1,10 +1,57 @@
 package com.company.GUI;
 
+import com.company.Model.DatabaseOperation;
+import com.company.Model.Student;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UserPage {
-    private JButton profile;
-    private JTextField reservedId;
-    private JTextField reservationDate;
-    private JTextField dueDate;
+    private JTextField itemId;
+    private JButton resButton;
+    private JButton historyButton;
+    private JPanel Panel;
+    private DatabaseOperation operation;
+    private Student student;
+
+    public UserPage(DatabaseOperation operation, Student student) {
+        this.operation = operation;
+        this.student = student;
+        resButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!itemId.getText().equals("")){
+                    int id = Integer.parseInt(itemId.getText());
+                    operation.reserveItemWithoutId(student.getStudentId(), id);
+                }else{
+                    JOptionPane.showMessageDialog(null, "Item ID is missing!");
+                }
+            }
+        });
+        historyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                HistoryTable table = new HistoryTable(operation.getStudentHistory(student.getStudentId()));
+                
+            }
+        });
+    }
+
+    public JPanel getPanel() {
+        return Panel;
+    }
+
+
+    public static void main(String[] args) {
+        DatabaseOperation op = new DatabaseOperation();
+        JFrame frame = new JFrame();
+        Student student= new Student(22,"uu@ozu.edu.tr", "djkfndsg", null,false);
+        frame.setVisible(true);
+
+        UserPage sp = new UserPage(op, student);
+
+        frame.setContentPane(sp.getPanel());
+    }
+
 }
